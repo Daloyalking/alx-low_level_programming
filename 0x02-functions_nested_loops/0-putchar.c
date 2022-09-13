ab170fa0dfd3e4e10e8c2718c8faef0b6ecc196b
Yusuf Ridwan Ayomide <65985009+Daloyalking@users.noreply.github.com>
2022-09-13 19:49:19 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,4 @@
-
 #include "holberton.h"
-#include "main.h"
 
 /**
  * main - Entry point
@@ -10,14 +8,15 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
 	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 	return (0);
 }
