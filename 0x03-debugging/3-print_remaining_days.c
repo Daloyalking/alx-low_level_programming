3f5ce86f6be5ed05dc2f9322a9cb86c82d20f534
Yusuf Ridwan Ayomide <65985009+Daloyalking@users.noreply.github.com>
2022-09-15 20:25:21 +0100

    Update 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -15,8 +15,9 @@ if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
 {
 if (month >= 3 && day >= 60)
 {
-days++;
+day++;
 }
+
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
 }
