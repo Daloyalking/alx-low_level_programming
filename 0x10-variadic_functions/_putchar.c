75e7018116eee189fca2a30195fa2ae59052ea91
Yusuf Ridwan Ayomide <65985009+Daloyalking@users.noreply.github.com>
2022-10-13 22:07:11 +0100

    Update _putchar.c

--- 0x10-variadic_functions/_putchar.c
+++ 0x10-variadic_functions/_putchar.c
@@ -1,13 +1,30 @@
-#include <unistd.h>
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+/*
+ * File: variadic_functions.h
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0F-variadic_functions directory.
+ */
+
+#include <stdarg.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * struct printer - A new struct type defining a printer.
+ * @symbol: A symbol representing a data type.
+ * @print: A function pointer to a function that prints
+ *         a data type corresponding to symbol.
  */
-int _putchar(char c)
+typedef struct printer
 {
-	return (write(1, &c, 1));
-}
+	char *symbol;
+	void (*print)(va_list arg);
+
+} printer_t;
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
